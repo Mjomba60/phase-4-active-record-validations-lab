129ded626867c8f562dacc088a838d1973754fc7
George Samuel <geosam2708@gmail.com>
2023-06-24 00:00:31 +0300

    Completed assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: ['Fiction', 'Non-Fiction']}
+
+    validate :title_sufficiency
+
+    private
+    def title_sufficiency
+        if !title&.match?(/Won't Believe|Secret|Top\d|Guess/)
+            errors.add(:title, 'Should consist of either "Won\'t Believe", "Secret", "Top [number]" or "Guess"' )
+        end
+    end
 end
